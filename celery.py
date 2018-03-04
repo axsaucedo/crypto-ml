86f12bb05a92a2470e1b7295ccfe7e0bd5af69ba
Alejandro Saucedo <axsauze@gmail.com>
2018-03-04 13:16:48 +0000

    added

--- celery.py
+++ celery.py
@@ -1,6 +1,26 @@
 from celery import Celery
 import datetime, time
 import pickle
+from fbprophet import Prophet
+from sklearn import linear_model
+
+models = {}
+
+def linear(dates, prices):
+    dates = np.reshape(dates, (len(dates),1))
+    prices = np.reshape(prices, (len(prices),1))
+    linear_mod = linear_model.LinearRegression()
+    linear_mod.fit(dates, prices)
+
+def svr(dates, prices):
+
+def prophet(prices):
+    m = Prophet()
+    s = 10
+    m.fit(prices)
+    future = m.make_future_dataframe(periods=30)
+    forecast = m.predict(future)
+    return forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']][-s:]
 
 def dump(o):
     return pickle.dumps(o).hex()
@@ -11,7 +31,7 @@ def load(o):
 app = Celery('tasks', backend='amqp://guest@localhost//', broker='amqp://guest@localhost//')
 
 @app.task
-def reg(p_model, p_x, p_y):
+def reg(model, p_x):
         x = load(p_x)
         y = load(p_y)
         model = load(p_model)
@@ -21,4 +41,11 @@ def reg(p_model, p_x, p_y):
         time.sleep(5) # (added this to show that work is being distributed)
         end_tme = datetime.datetime.now()
         return (dump(coef), start_tme, end_tme)
+
+if __name__ == '__main__':
+    df = pd.read_csv("data/crypto/Bitcoin.csv", 
+                        parse_dates=['Date']
+                        , usecols=["Date", "Close"])
+    prices = df[['Close']].values
+    
 
